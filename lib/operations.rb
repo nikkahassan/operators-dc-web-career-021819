6bc91786a47e9e65b06500d272528ae56879a9ba
Nikka Hassan <nikka.hassan@gmail.com>
2019-01-30 23:28:45 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,14 @@
 def unsafe?(speed)
-  if speed > 60 
-    puts true 
-    elsif speed < 40 
-    puts true 
+
+   if speed>=40 && speed<=60
+    false
   else
-    return false 
+    true
   end
 end
 
 def not_safe?(speed)
 
  	speed<=40 || speed>=60 ? true : false
-end	
-
+end
 
